2787e10ba5e59a6d85cf0d80dfa0c1577825d341
Felix <felix.famlarsson@gmail.com>
2022-10-07 12:35:47 +0200

    Added Kivy prototype

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 # TEINF-20
 # 25-11-2022
 # Travify
+
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+import random
+
+kivy.require('1.9.0')
+
+class Trippy(App):
+    def build(self):
+        return MyRoot()
+
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(1, 20))
+
+trippy_app = Trippy()
+trippy_app.run()
